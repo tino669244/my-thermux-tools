df5ddcd4f3e9f866de25a8b30cf23c115ef5dc02
Tino Ruine <tinoruine@gmail.com>
2025-12-09 14:24:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-// Fiasa tsotra miasa tsara
-function showMessage() {
-    document.getElementById("result").innerText = "Miasa tsara ny JavaScript!";
+function generateQR() {
+  const text = document.getElementById("textInput").value.trim();
+
+  if (!text) {
+    alert("Soraty aloha ny texte!");
+    return;
+  }
+
+  const qrUrl = "https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=" + encodeURIComponent(text);
+  document.getElementById("qrImage").src = qrUrl;
 }
