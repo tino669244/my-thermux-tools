a2afd67b34045a4a6529033b8e2e197fe16c61ea
Tino Ruine <tinoruine@gmail.com>
2025-12-30 10:47:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,16 @@
-function generateQR() {
-  const text = document.getElementById("textInput").value;
-  const img = document.getElementById("qrImage");
-
-  if (!text) {
-    alert("Ampidiro lien aloha");
-    return;
-  }
-
-  img.src =
-    "https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=" +
-    encodeURIComponent(text);
-
-  img.style.display = "block";
+function generateQR(){
+  const t=document.getElementById("textInput").value;
+  const img=document.getElementById("qrImage");
+  if(!t){alert("Ampidiro ny lien aloha");return;}
+  img.src="https://api.qrserver.com/v1/create-qr-code/?size=250x250&data="+encodeURIComponent(t);
+  img.style.display="block";
 }
 
-function downloadQR() {
-  const img = document.getElementById("qrImage");
-  if (!img.src) return;
-
-  const a = document.createElement("a");
-  a.href = img.src;
-  a.download = "QR_Tino_Andraina.png";
+function downloadQR(){
+  const img=document.getElementById("qrImage");
+  if(!img.src)return;
+  const a=document.createElement("a");
+  a.href=img.src;
+  a.download="QR_Tino_Andraina.png";
   a.click();
 }
